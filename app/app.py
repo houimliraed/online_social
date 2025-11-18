7e54f354b2107dfce1d32feff894bc42f287d75d
houimliraed <houimliraed@outlook.fr>
2025-11-18 04:32:15 +0100

    setup db

--- app/app.py
+++ app/app.py
@@ -1,2 +1,16 @@
-from fastapi import FastAPI
 
+from fastapi import FastAPI, HTTPException
+from typing import Optional
+from app.schemas import PostCreate,PostResponse
+
+
+
+app = FastAPI()
+
+
+# first endpoint
+@app.get('/')
+def root():
+    return{
+        "hello": "world"
+    }
